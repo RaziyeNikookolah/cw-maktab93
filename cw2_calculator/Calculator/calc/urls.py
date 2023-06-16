8123a8851731224e1185bbdde81a892bd6391c5f
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-16 11:21:35 -0600

    get form values in view to calculate

--- cw2_calculator/Calculator/calc/urls.py
+++ cw2_calculator/Calculator/calc/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.result, name='result')
+    path('result/', views.result, name='result')
 ]
--- cw2_calculator/Calculator/calc/views.py
+++ cw2_calculator/Calculator/calc/views.py
@@ -3,8 +3,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {"a": ""})
+    return render(request, 'index.html')
 
 
 def result(request):
-    return None
+    result_ = 0
+    operator = request.GET['operator']
+    num1 = request.GET['num1']
+    num2 = request.GET['num2']
+    print(num1, num2, operator)
+    if operator == "add":
+        result_ = num1 + num2
+    elif operator == "subtract":
+        result_ = num1 - num2
+    elif operator == "multiply":
+        result_ = num1 * num2
+    elif operator == "divide":
+        if num2 != 0:
+            result_ = num1 / num2
+    return render(request, 'result.html', {"result": result_})
