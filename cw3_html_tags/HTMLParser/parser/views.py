63b1175981ba3ef57dfbd12d867ad29c958aad1d
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-16 13:42:02 -0600

    having get and post request to one page, and cmplt it

--- cw3_html_tags/HTMLParser/parser/views.py
+++ cw3_html_tags/HTMLParser/parser/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def result(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+
+    html_tags = request.POST['html_input']
+    return HttpResponse(html_tags)
