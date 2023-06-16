6e0a2c1e1050b9f77445dde7465fe62bc26c906c
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-16 05:22:58 -0600

    impl author model

--- cw1_library/Library/books/models.py
+++ cw1_library/Library/books/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField()
