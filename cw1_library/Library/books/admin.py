3f8a30672c1b4e36d18816f8d6ef13d16a366c3b
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-16 06:44:08 -0600

    set urls

--- cw1_library/Library/books/admin.py
+++ cw1_library/Library/books/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 
-from models import Book, Author, Member
+from .models import Book, Author, Member
 
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Member)
 
-@admin.register(Book)
-class BookAdmin (admin.ModelAdmin):
-    list_display = ('title','publication_date','display_author')
-    list_filter = ('publication_date','authors')
-    search_fields = ('title','authors__name')
 
-    def display_authors(self,obj):
-        return f', '.join([author.name for author in obj.author.all()])
-    display_auhtors.short_description = 'Authors'
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'publication_date', 'display_author')
+#     list_filter = ('publication_date', 'authors')
+#     search_fields = ('title', 'authors__name')
+
+    # def display_authors(self, obj):
+    #     return f', '.join([author.name for author in obj.author.all()])
+    #
+    # display_auhtors.short_description = 'Authors'
--- cw1_library/Library/books/models.py
+++ cw1_library/Library/books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField()
@@ -7,6 +8,8 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField()
@@ -14,29 +17,29 @@ class Book(models.Model):
     description = models.TextField()
     isbn = models.CharField(max_length=13)
 
-
     def __str__(self):
         return self.title
 
-class Memeber(models.Model):
+
+class Member(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField()
     email = models.EmailField()
     contact_number = models.IntegerField()
     address = models.CharField(max_length=200)
-    book_borrowed = models.ManyToManyField(Book, through='BorrowedBook')
+    # book_borrowed = models.ManyToManyField(Book, through='BorrowedBook')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
-
-
-class BorrowedBook (models.Model):
-    member = models.ForeignKey(Memeber,on_delete=models.RESTRICT)
-    book = models.ForeignKey(Book, on_delete=models.RESTRICT)
-    borrowed_date = models.DateField()
-    return_date = models.DateField(null=True,blank=True)
-    deadline_date = models.DateField()
-    def __str__(self):
-        return f'{self.member} - {self.book}'
+#
+# class BorrowedBook(models.Model):
+#     member = models.ForeignKey(Memeber, on_delete=models.RESTRICT)
+#     book = models.ForeignKey(Book, on_delete=models.RESTRICT)
+#     borrowed_date = models.DateField()
+#     return_date = models.DateField(null=True, blank=True)
+#     deadline_date = models.DateField()
+#
+#     def __str__(self):
+#         return f'{self.member} - {self.book}'
