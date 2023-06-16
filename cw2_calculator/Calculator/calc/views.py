9859dae0b716088f940807706a4ed827ba910a92
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-16 09:50:10 -0600

    cnct urls and views for first user experience

--- cw2_calculator/Calculator/calc/views.py
+++ cw2_calculator/Calculator/calc/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
+    return render(request, 'index.html', {"a": ""})
+
+
+def result(request):
     return None
