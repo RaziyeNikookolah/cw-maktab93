1150634d4aa76a78a598139bb14820a656b62941
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-16 05:26:36 -0600

    impl member model

--- cw1_library/Library/books/models.py
+++ cw1_library/Library/books/models.py
@@ -13,6 +13,20 @@ class Book(models.Model):
     authors = models.ManyToManyField(Author)
     description = models.TextField()
     isbn = models.CharField(max_length=13)
+    
+
 
     def __str__(self):
         return self.title
+
+class Memeber(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    email = models.EmailField()
+    contact_number = models.IntegerField()
+    address = models.CharField(max_length=200)
+    book_borrowed = models.ManyToManyField(Book, through='BorrowedBook')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
