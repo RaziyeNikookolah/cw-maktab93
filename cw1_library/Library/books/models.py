fe33dbccdc212b264e0255e08e5bdbb3d826ad78
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-16 05:21:58 -0600

    impl book model

--- cw1_library/Library/books/models.py
+++ cw1_library/Library/books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    description = models.TextField()
+    isbn = models.CharField(max_length=13)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
