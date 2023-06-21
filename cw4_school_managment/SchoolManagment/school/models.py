fd11e87ad64df726eaaebf218ad1bcf25e4816b7
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-21 06:41:41 +0330

    add str method in all models

--- cw4_school_managment/SchoolManagment/school/models.py
+++ cw4_school_managment/SchoolManagment/school/models.py
@@ -11,17 +11,24 @@ class Student(models.Model):
     gender = models.CharField("gender of the student", choices=GENDER)
     address = models.TextField("the address of the student", required=True)
     email = models.EmailField("email address of the student", required=True, unique=True)
-    teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE,related_name="students")
-    student_class=models.ForeignKey("Class",on_delete=models.CASCADE,related_name="student",unique=True)
+    teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE, related_name="students")
+    student_class = models.ForeignKey("Class", on_delete=models.CASCADE, related_name="student", unique=True)
+
+    def __str__(self):
+        return f'{self.name} {self.roll_number}'
 
 
 class Teacher(models.Model):
     name = models.CharField("name of the teacher", required=True, max_length=100)
-    subject = models.CharField("the subject taught by the teacher", required=True, max_length=100)
+    # subject = models.CharField("the subject taught by the teacher", required=True, max_length=100)
+    subject = models.ForeignKey("Subject", on_delete=models.CASCADE, related_name="teachers")
     date_of_birth = models.DateField("birthdate of the teacher", required=True)
     gender = models.CharField("gender of the teacher", choices=GENDER, required=True)
     address = models.TextField("address of the teacher", required=True)
     email = models.EmailField("email address of the teacher", required=True, unique=True)
+
+    def __str__(self):
+        return f'{self.name} {self.subject}'
 
 
 class Class(models.Model):
@@ -29,13 +36,18 @@ class Class(models.Model):
     section = models.CharField("section of the class(e.g.,\"A\", \"B\")", required=True)
     start_time = models.TimeField("start time of the class", required=True)
     end_time = models.TimeField("end time of the class", required=True)
-    teacher=models.ForeignKey("Teacher",on_delete=models.CASCADE,related_name="class",unique=True)
+    teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE, related_name="class", unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Subject(models.Model):
     name = models.CharField("name of the subject", required=True, max_length=100)
     code = models.CharField("unique code for the subject", required=True, max_length=10)
-    teacher=models.ForeignKey("Teacher",on_delete=models.CASCADE,related_name="subjects")
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Exam(models.Model):
@@ -43,17 +55,26 @@ class Exam(models.Model):
     date = models.DateField("date of the exam", required=True)
     exam_class = models.ForeignKey(Class, on_delete=models.CASCADE, required=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Result(models.Model):
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE, required=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE, required=True)
-    marks = models.IntegerField("marks obtained by the student")
+    marks = models.IntegerField("marks obtained by the student")  # should not be list?
+
+    def __str__(self):
+        return f'{self.marks}'
 
 
 class Attendance(models.Model):
     date = models.DateField("date for whitch the attendance is recorded", required=True)
     student = models.ForeignKey(Student, "student whose attendance is recorded", on_delete=models.CASCADE())
     status = models.CharField("attendance status", choices=ATTENDANCE_STATUS)
+
+    def __str__(self):
+        return f'{self.status}'
 
 
 class Assignment(models.Model):
@@ -62,11 +83,17 @@ class Assignment(models.Model):
     due_date = models.DateField("due date for the assignment", required=True)
     subject = models.ForeignKey(Subject, "subject for which the assignment is given", required=True)
 
+    def __str__(self):
+        return f'{self.title}'
+
 
 class Notice(models.Model):
     title = models.CharField("title of the notice", required=True, max_length=100)
     description = models.TextField("description/details of the notice", required=True)
     date = models.DateField("date on which the notice is posted", required=True)
+
+    def __str__(self):
+        return f'{self.title}'
 
 
 class LibraryBook(models.Model):
@@ -74,3 +101,6 @@ class LibraryBook(models.Model):
     author = models.CharField("author of the book", required=True, max_length=100)
     publication_date = models.DateField(" publication date of the book", required=True)
     availability_status = models.BooleanField("availability status of the book")
+
+    def __str__(self):
+        return f'{self.title}'
