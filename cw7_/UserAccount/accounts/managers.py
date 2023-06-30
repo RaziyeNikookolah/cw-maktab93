0c56b86c82cbea20abff7a9a777882da295c3102
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-30 21:55:30 +0330

    cmplt cw

--- cw7_/UserAccount/accounts/managers.py
+++ cw7_/UserAccount/accounts/managers.py
@@ -3,14 +3,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where phone_number is the unique identifiers
-    for authentication instead of usernames.
-    """
+
     def create_user(self, phone_number, password, **extra_fields):
-        """
-        Create and save a user with the given phone_number and password.
-        """
+
         if not phone_number:
             raise ValueError(_("The phone_number must be set"))
         user = self.model(phone_number=phone_number, **extra_fields)
@@ -19,9 +14,7 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, phone_number, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given phone_number and password.
-        """
+
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
